357eb07f89b3909e105195c8e9bee28f59e0683c
hyunyoung <lhy76927@gmail.com>
2020-11-04 17:34:37 +0900

    oracle

--- hr1104.sql
+++ hr1104.sql
@@ -22,7 +22,21 @@ SELECT last_name, salary FROM employees WHERE salary >= 12000;
 --연봉 5000에서 12000의 범위 이외의 사람들의 LAST_NAME 및 연봉 조회
 SELET last_name, salary from employees whers salary < 5000 or salary > 12000;
 
+--연산자를 이용한 조회
 
+--산술연산자"sal*12 한 금액에 3600 인 데이터 조회
+select * from emp where sal*12=36000;
 
+select * from emp where sal > 3000;
+salect * form emp where sal >= 3000;
 
+select * from emp where ename >= 'f';
 
+--2500이상이고
+select * from emp where sal >=2500 AND job='ANALYST';
+
+select * from emp WHERE job='manager' or job='SALESMAN' or job='clerk';
+
+select * from emp where sal!=3000;
+
+SELECT * FROM emp where SAL!=3000;
